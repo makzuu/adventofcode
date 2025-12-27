46deb39ac5adae91d788b238e521b3c6dc78c76a
makzuu <mr.greenmousth@gmail.com>
2025-12-27 16:45:00 -0300

    aoc 2025 day 1

---
+++ 2025/01/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int rotate(int dial, int rotation) {
+    return ((dial + rotation) % 100 + 100) % 100;
+}
+
+int main() {
+    int dial = 50;
+    int password = 0;
+
+    int c;
+    char buff[50];
+    int i = 0;
+    while ((c = getchar()) != EOF) {
+	if (c != '\n') {
+	    buff[i++] = c;
+	} else {
+	    buff[i] = '\0';
+	    i = 0;
+
+	    char direction = buff[0];
+	    int distance = atoi(buff + 1);
+
+	    if (direction == 'L') {
+		dial = rotate(dial, -distance);
+	    } else {
+		dial = rotate(dial, distance);
+	    }
+	    if (dial == 0) {
+		password++;
+	    }
+	}
+    }
+    printf("%d\n", password);
+}
